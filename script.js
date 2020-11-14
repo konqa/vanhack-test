860218eb2f9d37a9723edba4c1f3aa4ca3004293
Victor Bagu <bagu@Victors-MBP.home>
2020-11-14 22:49:45 +0200

    add localstorage to apply function

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function apply(id) {
-  alert(id);
+  // check events user has applied for
+  let currentEvents = localStorage.getItem("events_applied")
+    ? JSON.parse(localStorage.getItem("events_applied"))
+    : [];
+
+  if (!currentEvents.includes(id)) {
+    // add new event ID to array
+    currentEvents.push(id);
+
+    // add to localstorage
+    localStorage.setItem("events_applied", JSON.stringify(currentEvents));
+  }
 }
