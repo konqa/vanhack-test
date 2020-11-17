51dd0e30d31419a1d923b10f5e62c64ce8c1c3f9
Victor Bagu <bagu@Victors-MBP.home>
2020-11-17 23:55:05 +0200

    check if id is in applied and hide on load

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let currentAppliedEvents = localStorage.getItem("events_applied")
+  ? JSON.parse(localStorage.getItem("events_applied"))
+  : [];
+
+currentAppliedEvents.map((id) => {
+  console.log(id);
+  // hide apply button on specific event
+  document.querySelector(`#apply-btn-${id}`).style.display = "none";
+
+  // show applied div
+  document.querySelector(`#applied-${id}`).style.display = "flex";
+});
+
 function apply(id) {
   // check events user has applied for
   let currentAppliedEvents = localStorage.getItem("events_applied")
@@ -14,6 +27,12 @@ function apply(id) {
       JSON.stringify(currentAppliedEvents)
     );
 
+    // hide apply button on specific event
+    document.querySelector(`#apply-btn-${id}`).style.display = "none";
+
+    // show applied div
+    document.querySelector(`#applied-${id}`).style.display = "flex";
+  } else if (currentAppliedEvents.includes(id)) {
     // hide apply button on specific event
     document.querySelector(`#apply-btn-${id}`).style.display = "none";
 
